a7bf949f02e5949d7a0cddb86c03611a8536e45b
haoxiangpeng <3150102255@zju.edu.cn>
2017-05-17 21:46:53 +0800

    finish bucket

--- BPlusTree.h
+++ BPlusTree.h
@@ -4,7 +4,38 @@
 
 #pragma once
 
-class BPlusTree {
+#include <string>
+#include <vector>
 
+//template<typename Key, typename Value>
+class Node;
+
+//template<class Node>
+class Bucket {
+public:
+    Bucket(int);
+
+    bool is_root;
+
+    bool is_leaf;
+    bool is_inner;
+
+    bool insert_to_cur_bucket(Node);
+
+    void split_bucket();
+
+    void print_bucket();
+
+private:
+    int degree;
+    Bucket *parent;
+    std::vector<Node> nodes;
+};
+
+//template<typename Key, typename Value>
+class Node {
+public:
+    std::pair<int, std::string> nodes;
+    std::vector<Bucket *> pointers;
 };
 
--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
-#include "bpt.h"
-#include <cstring>
-#include <ctime>
-#include <iostream>
-#include <map>
-#include <sstream>
+#include "BPlusTree.h"
 
 
 int main(int argc, char *argv[]) {
-    trees::BTree b_plus_tree();
+    Bucket bpt(4);
+    Node test_node;
+    test_node.nodes = std::make_pair(3, "hello");
+    bpt.insert_to_cur_bucket(test_node);
+    test_node.nodes = std::make_pair(4, "hello");
+    bpt.insert_to_cur_bucket(test_node);
+    test_node.nodes = std::make_pair(5, "hello");
+    bpt.insert_to_cur_bucket(test_node);
+    bpt.print_bucket();
     return 0;
 }
